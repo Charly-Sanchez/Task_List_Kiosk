59906bd257f207ce0f280ab8f9fa01d9725511cc
Charly-Sanchez <carlos_armando@colgalileoantigua.edu.gt>
2026-03-10 20:54:34 +0100

    Configura credenciales Firebase reales

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // Replace these values with your Firebase Web App config.
 export const firebaseConfig = {
-    apiKey: 'REPLACE_ME',
-    authDomain: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
-    storageBucket: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    appId: 'REPLACE_ME'
+    apiKey: 'AIzaSyCzOBfrmTfDFV7MUITKVkQe64_LeOxI-H4',
+    authDomain: 'task-list-kiosk.firebaseapp.com',
+    projectId: 'task-list-kiosk',
+    storageBucket: 'task-list-kiosk.firebasestorage.app',
+    messagingSenderId: '748498996203',
+    appId: '1:748498996203:web:c4f04e98ae42dc0a11a7ae'
 };
 
 export function isFirebaseConfigured() {
